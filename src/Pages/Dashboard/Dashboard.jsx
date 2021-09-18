2e84a0a7546c5e45b60c05d46cb94bc62348fdbf
ahsankhan201010 <ahsankhan201010@gmail.com>
2021-09-18 16:45:30 +0500

    socketio config

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import socket from '../../socket'
 
 const Dashboard = () => {
+    useEffect(() => {
+        //CDM
+        socket.connect()
+    },[])
     return (
         <div>
             <h1>Dashboard</h1>
